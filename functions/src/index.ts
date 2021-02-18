b4b0e5212e9e917c755765e85d083d0f092afa9f
Kodake <39713700+Kodake@users.noreply.github.com>
2021-02-18 01:10:06 -0400

    Initial changes about CrudFunctionsAPI

---
+++ functions/src/index.ts
@@ -0,0 +1,17 @@
+import * as functions from "firebase-functions";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+    response.json({
+        message: "Hello World from Firebase!"
+    });
+});
+
+export const helloFirebase = functions.https.onRequest((request, response) => {
+    response.json({
+        message: "Hello Firebase!"
+    });
+});
